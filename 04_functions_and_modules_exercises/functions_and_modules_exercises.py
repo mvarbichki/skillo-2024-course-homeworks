36b13037ba0de34e1bf94d40490ffd7eba4f3566
mvarbichki <116360901+mvarbichki@users.noreply.github.com>
2024-01-31 15:53:27 +0200

    Functions and modules homework dir adding

---
+++ 04_functions_and_modules_exercises/functions_and_modules_exercises.py
@@ -0,0 +1,20 @@
+"""Problem 0:
+Complete the following function so that it returns the sum of the elements in the list passed as an argument:
+
+def sum_elements(arr):
+result = 0
+#insert code here
+return result
+
+# Tests
+print(sum_elements([1,2,3,4])
+print(sum_elements([0])
+print(sum_elements([])
+print(sum_elements([-1, 1])
+"""
+
+
+def sum_elements(arr):
+    # sum array elements
+    result = sum(arr)
+    return result
