ae0a7c522ab0100a491cde0d45d731f70c3013b7
mvarbichki <116360901+mvarbichki@users.noreply.github.com>
2024-01-31 17:04:22 +0200

    Functions and modules problem 1 solution

--- 04_functions_and_modules_exercises/functions_and_modules_exercises.py
+++ 04_functions_and_modules_exercises/functions_and_modules_exercises.py
@@ -18,3 +18,25 @@ def sum_elements(arr):
     # sum array elements
     result = sum(arr)
     return result
+
+
+"""Problem 1: Simple Calculator Function Define a function called `simple_calculator` that takes two numbers and an 
+operator ('+', '-', '*', or '/') as arguments and returns the result of the operation."""
+# operator module
+import operator
+
+
+def simple_calculator(a, b, given_operator):
+    # adds the needed operators in dict
+    operators = {
+        "+": operator.add,
+        "-": operator.sub,
+        "*": operator.mul,
+        "/": operator.truediv
+    }
+    # assigns the desired operator funct (the dict value) via the dict key (the given_operator argument)
+    operator_funct = operators[given_operator]
+    # returns the executed operator function
+    return operator_funct(a, b)
+
+# print(simple_calculator(5, 2, "/"))
