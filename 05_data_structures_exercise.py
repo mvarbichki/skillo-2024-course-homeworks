c21663d7f1f6549f5f5882740d70fa013ec134c2
mvarbichki <116360901+mvarbichki@users.noreply.github.com>
2024-02-02 12:21:50 +0200

    Data Structures problem 2 solution

--- 05_data_structures_exercise.py
+++ 05_data_structures_exercise.py
@@ -8,4 +8,17 @@ def append_range_to_lst(arr):
     return arr
 
 
-print(append_range_to_lst(lst))
+# print(append_range_to_lst(lst))
+
+"""Problem 2. Reverse the order of the elements in the list from problem 1 and print the result."""
+
+
+def reverse_lst(arr):
+    arr.reverse()
+    return arr
+
+
+reversed_lst = reverse_lst(append_range_to_lst(lst))
+print(reversed_lst)
+
+
