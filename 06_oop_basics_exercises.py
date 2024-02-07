b82ea35edef937c17ef52cc29335a46aee1a857d
mvarbichki <116360901+mvarbichki@users.noreply.github.com>
2024-02-07 15:16:15 +0200

    OOP basics problem 1 solution

--- 06_oop_basics_exercises.py
+++ 06_oop_basics_exercises.py
@@ -17,6 +17,28 @@ class Person:
 person_one = Person("Sam", "male", 185, "brown")
 person_two = Person("Ema", "female", 155, "blue")
 
-print(person_one)
-print(person_two)
+# print(person_one)
+# print(person_two)
 
+"""Problem 1: Define a class "Email" with special methods "__eq__" and "__ne__" to compare two email addresses. Test 
+the equality and inequality operators with different email instances."""
+
+
+class Email:
+
+    def __init__(self, email):
+        self.email = email
+
+    def __eq__(self, other):
+        return self.email == other
+
+    def __ne__(self, other):
+        return self.email != other
+
+
+email_one = Email("astefanov@somemail.com")
+email_two = Email("mivanova@otehrmail.com")
+
+print(f"Is emails same? {email_one == email_two}")
+print(f"Is the email different? {email_one != email_two}")
+print(f"Is emails same? {email_two == email_two}")
