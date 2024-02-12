93e1f62d2b61990e8a97cef6899aa61a514a8e0f
mvarbichki <116360901+mvarbichki@users.noreply.github.com>
2024-02-12 13:48:50 +0200

    OOP exercise Online Movie Library part 3

--- 07_oop_exercise/Online Movie Library/movie_library.py
+++ 07_oop_exercise/Online Movie Library/movie_library.py
@@ -13,96 +13,7 @@ Start with designing your classes and entities in the project.
  recommendations, marking a movie as watched, rating a movie, listing all watched movies and so on.
 3. Implement tests cases accordingly"""
 
-"""
-class Movie
-movie_id:{
-    "name": "Rambo",
-    "description": "action",
-    "rating": avr([{user_id: 3, user_id: 5}])
-    }
 
-__init__
-    id
-    name
-    rating
-
-
-
-
-class Movie library
-all_movies ={
-    Movie obj
-}
-
-
-
-@admin
-def add/remove_movie to/from all_movies(id)
-    if i
-
-
-@user
-def calculate_rating()
-    return avr(all_movies.get("ratings))
-    
-@user
-def show_movie_info()
-    name/descr/calculate_rating()
-
-@user
-def show_recomended()
-    show last 3 added movie in all_movies
-
-
-
-
-IF ADMIN
--add movie -user_id  movie_id/name/description/rating ADD to all_movies
--remove movie - user_id, movie_id remove from all_movies
-
-
-IF USER
--if subscibed:
-    -accses all_movie | rating | description
-    - allowing rate movie ONCE
-
-
-
-clas User():
-
-    def select_person(user, list)
-    if user in list:
-        return user
-    else:
-        no such user    
-
-class Admins(User):
-   admn_list =  {id: name
-                }
-    
-    id
-    name
-    
- 
-
-class Subscriber:
-    sub_lst = {
-                id:{
-                    name: Pencho,
-                    status: subscribed
-                    },
-                id:{
-                    name: Toncho,
-                    status: subscribed
-                    }  
-                }
-    
-    
-    id
-    name
-    status sub/unsub
-    
-"""
 from movies import Movie
 from users import Administrator, Subscribers
 
@@ -113,55 +24,84 @@ class OnlineMovieLibrary:
         self.library_url = library_url
         self.__all_movies = dict()
 
-    def getter_movies(self):
+    # everyone could see what library contains
+    def show_all_lib_movies(self):
         return self.__all_movies
 
     def __find_movie(self, movie_name: str):
         if movie_name in self.__all_movies.keys():
             return movie_name
 
-    def add_movie_to_library(self, movie: dict, admin_name: str):
+    def add_movie_to_library(self, movie: dict, admin_user: str):
         # adds movie if provide admin
-        admin_exist = admin.if_admin(admin_name)
+        admin_exist = admin.if_user(admin_user)
         if admin_exist:
             self.__all_movies.update(movie)
+            return f"{movie.keys()} added successfully"
+        else:
+            return "Incorrect admin"
 
     def remove_movie_from_library(self, movie_name: str, admin_name: str):
-        admin_exist = admin.if_admin(admin_name)
+        admin_exist = admin.if_user(admin_name)
         if admin_exist:
             # if movie exist allow removing
             if self.__find_movie(movie_name):
                 del self.__all_movies[movie_name]
+                return f"{movie_name} removed"
+        else:
+            return "Incorrect admin "
+
+    def show_recommended_movies(self, subscriber):
+        # converts dict to list and show the last 3 elements
+        recently_added_movies = list(self.__all_movies.items())[-3:]
+        # any registered sub can see recommended movies
+        if subscribers.if_user(subscriber):
+            return f"Recommended: {recently_added_movies}"
+        else:
+            return f"You need to create account"
+
 
     # TODO show movie
-    #   def show_movie_info()
-    #   def show_recommended_movies()
+    #   def watch
+    #   def add_to_fav
+    #   def give_rating
     #   def calculate_rating()
 
 
 library = OnlineMovieLibrary("Movie world", "www.movieworld.com")
 admin = Administrator()
-admin.add_admin("Stefan")
+print(admin.create_account("someadmin"))
+print(admin.create_account("someadmin"))
+subscribers = Subscribers()
 
 # create movies obj
-m1 = Movie("Rambo-1989", "war, action")
+m1 = Movie("Rambo-1994", "war, action")
 m2 = Movie("Bad Boys-1983", "action")
 m3 = Movie("It-2017", "horror")
 m4 = Movie("Bad Boys-1995", "action")
 m5 = Movie("Love in the air-2024", "romantic, drama")
+m_six = Movie("Naruto-2006/2018", "anime, series")
 
-library.add_movie_to_library(m1.create_movie(), "Stefan")
-library.add_movie_to_library(m2.create_movie(), "Stefan")
-library.add_movie_to_library(m3.create_movie(), "Stefan")
-library.add_movie_to_library(m4.create_movie(), "Stefan")
-library.add_movie_to_library(m5.create_movie(), "Stefan")
+print(library.add_movie_to_library(m1.create_movie(), "someadmin"))
+print(library.add_movie_to_library(m2.create_movie(), "someadmin"))
+print(library.add_movie_to_library(m3.create_movie(), "someadmin"))
+print(library.add_movie_to_library(m4.create_movie(), "someadmin"))
+print(library.add_movie_to_library(m5.create_movie(), "someadmin"))
+print(library.add_movie_to_library(m_six.create_movie(), "someadmin"))
+print(library.add_movie_to_library(m_six.create_movie(), "someadminp"))
 
-print(library.getter_movies())
-library.remove_movie_from_library("Rambo-1989", "Stefan")
-print(library.getter_movies())
+print(library.show_all_lib_movies())
+print(library.remove_movie_from_library("Rambo-1994", "someadmin"))
+print(library.show_all_lib_movies())
 
-subscribers = Subscribers()
-subscribers.add_subscriber("someuser1", True)
-subscribers.add_subscriber("someuser2", False)
+print(subscribers.create_account("someuser1", "subscribed"))
+print(subscribers.create_account("someuser2", "trial"))
+print(subscribers.create_account("someuser3", "not subscribed"))
+print(subscribers.create_account("someuser3", "not subscribed"))
 
-print(subscribers.is_subscribed("someuser2"))
+print(library.show_recommended_movies('someuser'))
+print(library.show_recommended_movies('someuser3'))
+
+
+
+
--- 07_oop_exercise/Online Movie Library/users.py
+++ 07_oop_exercise/Online Movie Library/users.py
@@ -1,28 +1,52 @@
-class Administrator:
+from abc import ABC, abstractmethod
+
+
+class User(ABC):
+
+    @abstractmethod
+    def create_account(self, *args):
+        pass
+
+    @abstractmethod
+    def if_user(self, username):
+        pass
+
+
+class Administrator(User):
 
     def __init__(self):
-        self.__admin_list = []
+        self.__admin_list = set()
 
-    def add_admin(self, admin_name: str):
-        self.__admin_list.append(admin_name)
+    def create_account(self, username: str):
+        if username not in self.__admin_list:
+            self.__admin_list.add(username)
+            return f"{username} created"
+        else:
+            return f"{username} already exist"
 
-    def if_admin(self, admin_name):
-        if admin_name in self.__admin_list:
-            return admin_name
+    def if_user(self, username: str):
+        if username in self.__admin_list:
+            return username
 
 
-class Subscribers:
+class Subscribers(User):
 
     def __init__(self):
         self.__subscribers_list = dict()
 
-    def add_subscriber(self, username: str, subscribed: bool):
-        self.__subscribers_list.update({username: subscribed})
+    def create_account(self, username: str, subscribed: str):
+        if username not in self.__subscribers_list.keys():
+            self.__subscribers_list.update({username: subscribed})
+            return f"{username} created"
+        else:
+            return f"{username} already exist"
 
-    # dict guarantee uniq usernames as keys
-    def if_subscriber(self, username: str):
-        if username in self.__subscribers_list.keys():
+    def if_user(self, username: str):
+        if username in self.__subscribers_list:
             return username
 
     def is_subscribed(self, username: str):
-        return self.__subscribers_list.get(username)
+        if self.__subscribers_list.get(username) in ["subscribed", "trial"]:
+            return True
+        else:
+            return False
