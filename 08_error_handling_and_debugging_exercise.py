ba182918bfec4289f60f9099838a56cde0f1aa8a
mvarbichki <116360901+mvarbichki@users.noreply.github.com>
2024-02-15 16:45:35 +0200

    Error handling and debugging problem 1 solution and small changes

--- 08_error_handling_and_debugging_exercise.py
+++ 08_error_handling_and_debugging_exercise.py
@@ -9,7 +9,7 @@ print(f"\nThe income today was £{income:0.2f}")"""
 
 
 # custom-made class for handling negative int
-class ZeroOrNegative(Exception):
+class NegativeValue(Exception):
     def __init__(self, message):
         super().__init__(message)
 
@@ -18,6 +18,7 @@ items = {"Coffee": 2.2, "Tea": 1.5, "Chocolate": 2.5}
 
 
 def income_calculation(arr: dict):
+    # moves income out of for cycle
     income = 0
     # handles wrong input type
     try:
@@ -25,13 +26,34 @@ def income_calculation(arr: dict):
             qty = int(input(f"How many {item}s have you sold? "))
             # handles negative values, but allows 0
             if qty < 0:
-                raise ZeroOrNegative("Input can not negative")
+                raise NegativeValue("Input can not negative")
             income += qty * arr[item]
         return f"\nThe income today was £{income:0.2f}"
     except ValueError:
         return "You have to enter an integer"
-    except ZeroOrNegative as zon:
-        return zon
+    except NegativeValue as nv:
+        return nv
 
 
 # print(income_calculation(items))
+
+"""Problem 1:
+Rewrite the following function so it is exception safe
+def get_integer_input():
+num = int(input("Enter an integer: "))
+return num
+number = get_integer_input()
+print("You entered:", number)"""
+
+
+def get_integer_input():
+    # try/except to check for correct type
+    try:
+        num = int(input("Enter an integer: "))
+        return num
+    except ValueError:
+        return "Wrong type"
+
+
+number = get_integer_input()
+# print("You entered:", number)
