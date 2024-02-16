b1e63eec126d415f7b8cdabccc53ee7cf441dcba
mvarbichki <116360901+mvarbichki@users.noreply.github.com>
2024-02-16 15:05:09 +0200

    Files manipulation problem 2 solution

--- 09_files_manipulation_exercises/files_manipulation_exercises.py
+++ 09_files_manipulation_exercises/files_manipulation_exercises.py
@@ -4,8 +4,22 @@ sum."""
 
 def sum_file(file_name):
     with open(file_name, "r") as f:
-        res = [int(num) for num in f]
+        res = [int(row) for row in f]
     return sum(res)
 
 
 # print(sum_file("numbers.txt"))
+
+
+"""2. Write a program that reads a text file, "words.txt," and counts the number of words in it."""
+
+
+def count_words_file(file_name):
+    with open(file_name, "r") as f:
+        # splits converts each file's row in to list of words
+        # each list is counted via len and result appended in a list
+        # returns the counted words as sum of the list
+        return sum([len(row.split()) for row in f])
+
+
+print(count_words_file("words.txt"))
