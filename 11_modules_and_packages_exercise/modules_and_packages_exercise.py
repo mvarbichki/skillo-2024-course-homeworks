fab99e9a3769748a24f57ee7aea907ca3bbc35fc
mvarbichki <116360901+mvarbichki@users.noreply.github.com>
2024-02-26 15:26:18 +0200

    Modules and packages problems full solution and adding requirements file

---
+++ 11_modules_and_packages_exercise/modules_and_packages_exercise.py
@@ -0,0 +1,23 @@
+"""1. Install the requests package in python
+2. Make a request to `https://reqbin.com/echo/get/json`
+3. Save the response as a json file called response.json"""
+
+import requests
+
+
+url = "https://reqbin.com/echo/get/json"
+
+
+def make_request(url_str: str):
+    get_url = requests.get(url_str)
+    return get_url.text
+
+
+def write_to_json(url_content: str):
+    with open("response.json", "w") as f:
+        f.write(url_content)
+        return "Json file exported"
+
+
+url_extracted_content = make_request(url)
+print(write_to_json(url_extracted_content))
