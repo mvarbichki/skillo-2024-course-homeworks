d6acfa8966097b3b9d6a361638d6dd626ac43357
mvarbichki <116360901+mvarbichki@users.noreply.github.com>
2024-02-26 20:46:15 +0200

    Improving 01 solutions

--- 01_introduction_to_python_exercises.py
+++ 01_introduction_to_python_exercises.py
@@ -4,32 +4,32 @@
 arithmetic operations on them, printing the results."""
 
 
-def arithmetic_operators(num_one, num_two):
+def arithmetic_operators(num_one: float, num_two: float):
     # handle non str input
     try:
-        no = int(num_one)
-        nt = int(num_two)
-    except:
+        no = float(num_one)
+        nt = float(num_two)
+    except ValueError:
         return "Enter numbers"
 
-    addition = f"Addition:  {no + nt}"
-    subscription = f"Subscription: {no - nt}"
-    multiplication = f"Multiplication: {no * nt}"
-    exponential = f"Exponential: {no ** nt}"
+    addition = f"Addition: {'{:.2f}'.format(no + nt)}"
+    subscription = f"Subscription: {'{:.2f}'.format(no - nt)}"
+    multiplication = f"Multiplication: {'{:.2f}'.format(no * nt)}"
+    exponential = f"Exponential: {'{:.2f}'.format(no ** nt)}"
     # handle division on zero
     if no == 0 or nt == 0:
         division = "Division: Can not division on 0"
         modulus = "Modulus: Can not division on 0"
         floor_div = "Floor division: can not division on 0"
     else:
-        division = f"Division: {no / nt}"
-        modulus = f"Modulus: {no % nt}"
-        floor_div = f"Floor division: {no // nt}"
+        division = f"Division: {'{:.2f}'.format(no / nt)}"
+        modulus = f"Modulus: {'{:.2f}'.format(no % nt)}"
+        floor_div = f"Floor division: {'{:.2f}'.format(no // nt)}"
 
     return addition, subscription, multiplication, division, modulus, exponential, floor_div
 
 
-print(arithmetic_operators(input("Enter first numeric: "), input("Enter second numeric: ")))
+# print(arithmetic_operators(input("Enter first numeric: "), input("Enter second numeric: ")))
 
 """Problem 2:
 Write a Python script that creates 5 string variables of 5 bulgarian PINs ( ЕГН ) and 5 names in
@@ -45,12 +45,17 @@ p5 = ("9507722693", "Olga")
 vowel = "aeiouAEIOU"
 # persons list for check
 persons_list = [p1, p2, p3, p4, p5]
-# dict to store results
-vowel_dict = dict()
-for person in persons_list:
-    # checks person first letter for vowel
-    if person[1][0] in vowel:
-        # adds the person to the dict
-        vowel_dict[person[0]] = person[1]
 
-print(vowel_dict)
+
+# dict to store results
+def vowel_names(vow: str, p_list: list):
+    vowel_dict = dict()
+    for person in p_list:
+        # checks person first letter for vowel
+        if person[1][0] in vow:
+            # adds the person to the dict
+            vowel_dict[person[0]] = person[1]
+    return vowel_dict
+
+
+# print(vowel_names(vowel, persons_list))
