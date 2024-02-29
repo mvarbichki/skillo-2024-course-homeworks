a2e98989d0994de449625ae9db79d6c1e83e776c
mvarbichki <116360901+mvarbichki@users.noreply.github.com>
2024-02-29 12:17:40 +0200

    Modules and packages problem solution fix - now the request get the correct data and remove Json dump since the request is Json itself

--- 11_modules_and_packages_exercise/modules_and_packages_exercise.py
+++ 11_modules_and_packages_exercise/modules_and_packages_exercise.py
@@ -3,23 +3,33 @@
 3. Save the response as a json file called response.json"""
 
 import requests
-import json
-
 
 url = "https://reqbin.com/echo/get/json"
 
 
+# header argument allows the URL request
 def make_request(url_str: str):
-    get_url = requests.get(url_str)
-    return get_url.text
+    get_url = requests.get(url_str,
+                           headers={
+                               "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                                             "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 "
+                                             "Safari/537.36"
+                           }
+                           )
+    # if status code is successful return the url content
+    if get_url.status_code == 200:
+        return get_url.text
+    else:
+        return "Bad request"
 
 
 def write_to_json(url_content: str):
-    with open("response.json", "w") as f:
-        json_object = json.dumps(url_content)
-        f.write(json_object)
-        return "Json file exported"
+    if url_content != "Bad request":
+        with open("response.json", "w") as f:
+            f.write(url_content)
+            return "Json file exported"
+    return "URL request problem. Json export failed"
 
 
 url_extracted_content = make_request(url)
-print(write_to_json(url_extracted_content))
+# print(write_to_json(url_extracted_content))
